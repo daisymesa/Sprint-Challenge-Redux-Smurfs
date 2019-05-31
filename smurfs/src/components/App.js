7d13425bfdb4ebb97400309b5759ee545fefacb7
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-05-31 12:30:27 -0400

    Updating App.js with actions import and componentDidMount

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-// import
+
+import { getSmurfs } from '../actions';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -8,13 +10,24 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        
+        <div className='instructions'>
+          <h1>SMURFS! 2.0 W/ Redux</h1>
+          <div>Welcome to your Redux version of Smurfs!</div>
+          <div>Start inside of your `src/index.js` file!</div>
+          <div>Have fun!</div>
+        </div>
+
+
+
       </div>
     );
   }
