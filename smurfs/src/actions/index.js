fbf669e3a77833ea3799b79ab9a4919101f5b06e
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-05-31 13:52:14 -0400

    Added addSmurf to reducers

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -8,6 +8,10 @@ export const FETCHING = "FETCHING";
 export const SUCCESS = "SUCCESS";
 export const FAILURE = "FAILURE";
 
+export const ADD_SMURF = "ADD_SMURF";
+export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESS";
+export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE";
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -33,6 +37,25 @@ export const getSmurfs = () => dispatch => {
     .catch(err => {
         dispatch({
           type: FAILURE,
+          payload: err
+        });
+      });
+}
+
+export const addSmurf = () => dispatch => {
+  dispatch({ type: ADD_SMURF });
+  axios
+    .post('http://localhost:3333/smurfs')
+    .then(res => {
+        dispatch({
+          type: ADD_SMURF_SUCCESS,
+          payload: res.data
+
+        });
+      })
+    .catch(err => {
+        dispatch({
+          type: ADD_SMURF_FAILURE,
           payload: err
         });
       });
