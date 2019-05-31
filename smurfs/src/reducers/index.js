d8fb3d74f6446a17540ac1cc7d8da262a813900a
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-05-31 13:53:20 -0400

    Importing addSmurf actions to reducer index.js

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-import { FETCHING, SUCCESS, FAILURE } from '../actions';
+import { FETCHING, SUCCESS, FAILURE, ADD_SMURF, ADD_SMURF_SUCCESS, ADD_SMURF_FAILURE } from '../actions';
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
